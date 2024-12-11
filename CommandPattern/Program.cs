06246621901021cc5fca5b2a30470db3a91a4347
Felix Zhao <felix.zhao@consultant.udtrucks.com>
2024-12-11 16:11:49 +0800

    command pattern

--- CommandPattern/Program.cs
+++ CommandPattern/Program.cs
@@ -4,6 +4,13 @@ namespace CommandPattern
 {
     internal static class Program
     {
+        /*
+         * Command is a behavioral design pattern that turns a request into a stand-alone 
+         * object that contains all information about the request. This transformation 
+         * lets you pass requests as a method arguments, delay or queue a request’s 
+         * execution, and support undoable operations.
+         * 
+        */
         private static void Main()
         {
             var remote = new RemoteControl(3);
