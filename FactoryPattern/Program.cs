49e01886c72d7513cd7e2cd6f90155e8629b413d
Felix Zhao <felix.zhao@consultant.udtrucks.com>
2024-12-12 09:23:25 +0800

    factory pattern

--- FactoryPattern/Program.cs
+++ FactoryPattern/Program.cs
@@ -4,6 +4,11 @@ namespace FactoryPattern
 {
     static class Program
     {
+        /*
+         * Factory Method is a creational design pattern that provides an interface for 
+         * creating objects in a superclass, but allows subclasses to alter the type of 
+         * objects that will be created.
+         */
         static void Main()
         {
             Console.WriteLine("Yankees fan orders:");
