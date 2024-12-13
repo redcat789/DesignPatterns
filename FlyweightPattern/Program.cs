922e5786d260c100cf02c9f369d381ca8eb8cf95
Felix Zhao <felix.zhao@consultant.udtrucks.com>
2024-12-13 15:29:17 +0800

    Flyweight pattern

--- FlyweightPattern/Program.cs
+++ FlyweightPattern/Program.cs
@@ -2,6 +2,12 @@
 {
     static class Program
     {
+        /*
+         * The Flyweight Pattern is a structural design pattern used to optimize memory 
+         * usage by sharing common parts of an object between multiple instances. 
+         * It avoids creating multiple similar objects by storing shared data externally 
+         * and reusing objects.
+         */
         private static void Main()
         {
             var teaShop = new BubbleTeaShop();
